0f89c488235e4aa634bcc2c63f6e5ac9b9f877bb
Mila-95 <mila.kushchak@gmail.com>
2022-05-13 19:23:17 -0400

    4

--- Class25/BankAccountTester.java
+++ Class25/BankAccountTester.java
@@ -2,7 +2,7 @@ package com.syntax.Class25;
 
 public class BankAccountTester {
     public static void main(String[] args) {
-        BankAccount account=new BankAccount();
-        account.printBalance("Habib","Habib123456789");
+        BankAccount account = new BankAccount();
+        account.printBalance("Habib", "Habib123456789");
     }
 }
